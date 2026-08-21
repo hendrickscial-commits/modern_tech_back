e9955c25be90ad23326e5fa02b72e8132c168fa4
Kanya233 <khanya.njenjemane06@gmail.com>
2026-08-21 08:47:07 +0200

    added updated files

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import {
   getLeaveCon,
@@ -23,9 +25,13 @@ dotenv.config();
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Get all leave requests
 app.get("/leave", getLeaveCon);
